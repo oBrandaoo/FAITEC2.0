344379b73bfd78fee533e74f1a05b1d50ffc9e11
Rafael Brandão <rafaelbrandaoreis@gmail.com>
2026-03-13 19:12:46 -0300

    feat: add view for home page

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -2,20 +2,24 @@ package org.example;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
 
-        Label label = new Label("JavaFX funcionando 🚀");
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/view/home.fxml")
+        );
 
-        Scene scene = new Scene(label, 1000, 200);
+        Scene scene = new Scene(loader.load());
 
-        stage.setTitle("Teste JavaFX");
+        stage.setTitle("Cidade Melhor");
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
     }
 
