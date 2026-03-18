1e3f4ec481668054d15f338a2d78116234bda25b
GustavoPivotoGT <gustavorpl22@gmail.com>
2026-03-18 18:34:36 -0300

    Fixed button styles
    Arrumei os estilos dos botoes, e coloquei o efeito neles.

--- src/main/java/org/example/controller/HomeController.java
+++ src/main/java/org/example/controller/HomeController.java
@@ -8,12 +8,17 @@ import org.example.util.ScreenManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
 
 public class HomeController {
     @FXML
     private StackPane contentArea;
     @FXML
-    private Pane mainContainer;
+    private Button Inicio;
+    @FXML
+    private Button Reclamacoes;
+    @FXML
+    private Button Reclamacao;
     
     @FXML
     public void initialize() {
@@ -21,6 +26,34 @@ public class HomeController {
         ScreenManager.setMainContainer(contentArea);
 
         ScreenManager.loadScreen("HomeContent.fxml");
+
+        Inicio.setOnMouseEntered(e -> {
+            Inicio.setScaleX(1.1);
+            Inicio.setScaleY(1.1);
+        });
+        Inicio.setOnMouseExited(e -> {
+            Inicio.setScaleX(1.0);
+            Inicio.setScaleY(1.0);
+        });
+
+        Reclamacao.setOnMouseEntered(e -> {
+            Reclamacao.setScaleX(1.1);
+            Reclamacao.setScaleY(1.1);
+        });
+        Reclamacao.setOnMouseExited(e -> {
+            Reclamacao.setScaleX(1.0);
+            Reclamacao.setScaleY(1.0);
+        });
+
+        Reclamacoes.setOnMouseEntered(e -> {
+            Reclamacoes.setScaleX(1.1);
+            Reclamacoes.setScaleY(1.1);
+        });
+        Reclamacoes.setOnMouseExited(e -> {
+            Reclamacoes.setScaleX(1.0);
+            Reclamacoes.setScaleY(1.0);
+        });
+
 
     }
 
