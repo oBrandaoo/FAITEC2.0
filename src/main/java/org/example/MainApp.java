fe99c76c9c527d99fbfdc966f459f7e84b43bbe2
Rafael Brandão <rafaelbrandaoreis@gmail.com>
2026-07-09 21:30:24 -0300

    refactor: remove unused CSS stylesheet from MainApp

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -19,8 +19,6 @@ public class MainApp extends Application {
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
-        String css = this.getClass().getResource("/css/style.css").toExternalForm();
-        scene.getStylesheets().add(css);
         stage.setTitle("Cidade Melhor");
         stage.setScene(scene);
         stage.setFullScreen(true);
